f8ea03843ded78c0bca882049c7b110010281575
asmazovec <asmazovec@yandex.ru>
2020-04-25 16:28:53 +0700

    create header for the DECIMAL type

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-
-typedef signed long long int sll_int;
-typedef signed char sss_int;
-
-typedef struct {
-    sll_int num;
-    sss_int n;
-} DECIMAL;
+#include "DECIMAL.h"
 
 
 int main() {
