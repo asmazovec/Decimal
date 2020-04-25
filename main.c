a2139f4483d9adf21f56c361753178dce0d3df94
asmazovec <asmazovec@yandex.ru>
2020-04-25 16:20:55 +0700

    Definition of used types

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef signed long long int sll_int;
+typedef signed char sss_int;
+
+typedef struct {
+    sll_int num;
+    sss_int n;
+} DECIMAL;
+
+
+int main() {
+
+    return 0;
+}
